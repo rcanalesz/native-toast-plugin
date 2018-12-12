7729df1ef3398a001aaa987f183f1d0494acf5be
Rodrigo Raúl Canales Zorrilla <rcanalez@everis.com>
2018-12-12 16:56:29 -0500

    Toast plugin

---
+++ src/android/NativeToastPlugin.java
@@ -0,0 +1,47 @@
+package com.rcanales.cordova.plugin;
+// The native Toast API
+import android.widget.Toast;
+
+// Cordova-required packages
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class NativeToastPlugin extends CordovaPlugin {
+  private static final String DURATION_LONG = "long";
+
+  @Override
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
+      
+      // Verify that the user sent a 'show' action
+      if (action.equals("show")) {      
+        String message;
+        String duration;
+        try {
+            JSONObject options = args.getJSONObject(0);
+            message = options.getString("message");
+            duration = options.getString("duration");
+        } catch (JSONException e) {
+            callbackContext.error("Error encountered: " + e.getMessage());
+            return false;
+        }
+        // Create the toast
+        Toast toast = Toast.makeText(cordova.getActivity(), message,
+            DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);        
+        // Display toast
+        toast.show();
+        // Send a positive result to the callbackContext
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+        callbackContext.sendPluginResult(pluginResult);
+        return true;
+      }
+      else{
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
+        return false;
+      }
+  }
+}
---
+++ www/nativeToastPlugin.js
@@ -0,0 +1,21 @@
+// Empty constructor
+function NativeToastPlugin() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+NativeToastPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'NativeToastPlugin', 'show', [options]);
+}
+
+// Installation constructor that binds NativeToastPlugin to window
+NativeToastPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.nativeToastPlugin = new NativeToastPlugin();
+  return window.plugins.nativeToastPlugin;
+};
+cordova.addConstructor(NativeToastPlugin.install);
